24a9e0f4b8c999e27ee5cf83f69e3735c8474845
tutibase <dykun1784@gmail.com>
2024-11-19 17:16:00 +0300

    feat: implementation of FunSets

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -20,31 +20,31 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
+    def singletonSet(elem: Int): Set = external  => external == elem // множество из elem, с которым сравниваем "внешние" значения
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set = external => contains(s, external) || contains(t, external) // проверяем наличие в любом из множеств
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = external => contains(s, external) && contains(t, external) // проверяем наличие в обеих множествах
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = external => contains(s, external) && !contains(t, external) // наличие в `s` и отсутствие в `t`
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = intersect(s, p) //
   
 
   /**
@@ -57,23 +57,31 @@ object FunSets {
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true // раз дошли до границы, то все числа до этого прошли проверку
+      else if (diff(s, p)(a)) false // число есть в множестве и не прошло проверку
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound) // проверяем все числа [-bound; bound]
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = {
+      def iter(a: Int): Boolean = {
+        if (a > bound) false // раз дошли до границы, то все числа не подошли
+        else if (intersect(s, p)(a)) true // число есть в мн-ве `s` и прошло проверку
+        else iter(a + 1)
+      }
+      iter(-bound)
+    }
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+    def map(s: Set, f: Int => Int): Set = external => exists(s, a => f(a) == external) // проверяем существование такого элемента external,
+                                                                                       // что он есть в мн-ве f(s)
   
   /**
    * Displays the contents of a set
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,30 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  /*
+  val s1 = singletonSet(1)
+  val s2 = singletonSet(2)
+  val s3 = singletonSet(3)
+
+  val u12 = union(s1, s2)
+  val u123 = union(u12, s3)
+
+  val i23 = intersect(s2, s3)
+  val d13 = diff(s1, s3)
+
+  println("u12: " + FunSets.toString(u12))
+  println("u123: " + FunSets.toString(u123))
+  println("i23: " + FunSets.toString(i23))
+  println("d13: " + FunSets.toString(d13))
+
+  val isEven: Int => Boolean = x => x % 2 == 0
+  println("u123 evens: " + FunSets.toString(filter(u123, isEven)))
+
+  println("forall u123 greater than 2: " + forall(u123, x => x > 2))
+  println("exists u123 even: " + exists(u123, isEven))
+
+  val doubled = map(u123, x => x * 2)
+  println("doubled: " + FunSets.toString(doubled))
+   */
 }
