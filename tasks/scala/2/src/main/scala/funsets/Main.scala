ec259c0f696839e5f9c60664a0d7be501cbd27d7
StanleyStanMarsh <iastafev56@gmail.com>
2024-11-20 23:07:29 +0300

    add map functions

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -2,11 +2,25 @@ package funsets
 
 object Main extends App {
   import FunSets._
+  println("contains")
   println(contains(singletonSet(1), 1)) // true
-  println(contains(union(singletonSet(1), singletonSet(2)), 3)) // false
-  println(contains(union(singletonSet(1), singletonSet(2)), 2)) // true
-  println(contains(intersect(singletonSet(1), singletonSet(2)), 1)) // false
-  println(contains(intersect(union(singletonSet(1), singletonSet(2)), singletonSet(2)), 2)) // true
+
+  println("\nunion")
+  println(FunSets.toString(union(singletonSet(1), singletonSet(2)))) // {1, 2}
+
+  println("\nintersect")
+  println(FunSets.toString(intersect(singletonSet(1), singletonSet(2)))) // {}
+  println(FunSets.toString(intersect(union(singletonSet(1), singletonSet(2)), singletonSet(2)))) // {2}
+
+  println("\nforall")
+  println(FunSets.toString((x: Int) => x >= -20 && x % 3 == 0 && x <= 20))
   println(forall((x: Int) => x >= -20 && x % 3 == 0 && x <= 20, (x: Int) => x >= -20)) // true
+
+  println("\nexists")
+  println(FunSets.toString((x: Int) => x >= -20 && x % 3 == 0 && x <= 20))
   println(exists((x: Int) => x >= -20 && x % 3 == 0 && x <= 20, (x: Int) => x % 2 == 0)) // true
+
+  println("\nmap")
+  println(FunSets.toString((x: Int) => x >= 1 && x <= 3))
+  println(FunSets.toString(map((x: Int) => x >= 1 && x <= 3, (x: Int) => x * x))) // true
 }
