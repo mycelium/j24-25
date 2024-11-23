409208c086ed268fc6a53fa11301d5cd6a59c341
ArseniyBogdan <bogdan.senya@mail.ru>
2024-11-23 12:00:50 +0300

    Resolved 2 task

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,5 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  println(FunSets.toString(map(union(singletonSet(1), singletonSet(2)), x => x + 1)))
 }
