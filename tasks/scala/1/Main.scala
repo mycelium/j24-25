720fc7f28d1baf4f49316fa5280f3206edf755f8
Churova Sofya <sofacurova04@gmail.com>
2024-11-26 02:04:27 +0300

    First task solved

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,5 @@
 package recfun
-import common._
+//import common._
 
 
 object Main {
@@ -10,22 +10,46 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("______balance___")
+    print("'(()(()))())()()())))': ")
+    println(balance("(()(()))())()()())))".toList))
+    print("'(()()())': ")
+    println(balance("(()()())".toList))
+
+    println("money = 15, coins [1, 3, 2]")
+    println(countChange(15, List(1, 3, 2)))
+    println("money = 10, coins [1, 3, 2]")
+    println(countChange(10, List(1, 3, 2)))
+    println("money = 10, coins [1, 3, 2]")
+    println(countChange(5, List(1, 3, 2)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c < 0 || r < 0 || c > r) 0 // Некорректные входные данные
+    else if (c == 0 || c == r) 1 // Базовые случаи: крайние элементы
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
-  }
 
+  def balance(chars: List[Char]): Boolean = {
+    def count_balance (chs: List[Char], count: Int): Boolean = {
+      if (chs.isEmpty) {
+        if (count == 0) true
+        else false
+      }
+      else if (chs.head == '(')  count_balance(chs.tail, count + 1)
+      else if (chs.head == ')')  count_balance(chs.tail, count - 1)
+      else { count_balance(chs.tail, count) }
+    }
+    count_balance(chars, 0)
+  }
   /**
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
@@ -34,6 +58,14 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money < 0 || coins.isEmpty) 0
+    else {
+      if (money == 0) 1
+      else {
+        val firstCoin = coins.head
+        val remainingCoins = coins.tail
+        countChange(money - firstCoin, coins) + countChange(money, remainingCoins)
+      }
+    }
   }
 }
