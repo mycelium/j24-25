db54a8ec39a5cd32d31a61207ec022a76bbda332
Georgy Zoloev <gzoloev228@gmail.com>
2024-12-03 16:01:41 +0300

    feat: completed first lab

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,15 +15,23 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def loop(chars: List[Char], openCount: Int): Boolean = {
+      if (chars.isEmpty) openCount == 0
+      else if (chars.head == '(') loop(chars.tail, openCount + 1)
+      else if (chars.head == ')') openCount > 0 && loop(chars.tail, openCount - 1)
+      else loop(chars.tail, openCount)
+    }
+    loop(chars, 0)
   }
+
 
   /**
    * Exercise 3 Counting Change
@@ -33,6 +41,9 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
+
 }
