cc9a54e43c579025639a78cc9834fe77bdc723fd
GrigorievPetr <skyphias@gmail.com>
2024-12-03 20:57:41 +0300

    Added  scala tasks 1 and 2

---
+++ tasks/scala/1/Main.scala
@@ -0,0 +1,49 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c-1,r-1) + pascal(c, r-1)
+  }
+
+  /**
+   * Exercise 2 Parentheses Balancing
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def loop(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty) open == 0
+      else if (open < 0) false
+      else if (chars.head == '{'  chars.head == '['  chars.head == '(') loop(chars.tail, open + 1)
+      else if (chars.head == '}'  chars.head == ']'  chars.head == ')') loop(chars.tail, open - 1)
+      else loop(chars.tail, open)
+    }
+    loop(chars,0)
+  }
+
+  /**
+   * Exercise 3 Counting Change
+   * Write a recursive function that counts how many different ways you can make
+   * change for an amount, given a list of coin denominations. For example,
+   * there is 1 way to give change for 5 if you have coins with denomiation
+   * 2 and 3: 2+3.
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0) 0
+    else if (money == 0) 1
+    else if (coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+}
