e0d15469164fb8c9a6df844d8e6014e3923c1cf0
dwezis <emax151004@gmail.com>
2024-12-03 22:15:42 +0300

    Task2
    Second task

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -20,32 +20,32 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
-  
+  def singletonSet(elem: Int): Set = (x: Int) => x == elem
+
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
-  
+  def union(s: Set, t: Set): Set = (x: Int) => s(x) || t(x)
+
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
-  
+  def intersect(s: Set, t: Set): Set = (x: Int) => s(x) && t(x)
+
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
-  
+  def diff(s: Set, t: Set): Set = (x: Int) => s(x) && !t(x)
+
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
-  
+  def filter(s: Set, p: Int => Boolean): Set = (x: Int) => s(x) && p(x)
+
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -55,26 +55,26 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (s(a) && !p(a)) false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound)
   }
-  
+
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, a => !p(a))
+
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+  def map(s: Set, f: Int => Int): Set = (x: Int) => s(f(x))
+
   /**
    * Displays the contents of a set
    */
@@ -86,7 +86,7 @@ object FunSets {
   /**
    * Prints the contents of a set on the console.
    */
-  def printSet(s: Set) {
+  def printSet(s: Set): Unit = {
     println(toString(s))
   }
 }
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,42 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  val set1: Set = FunSets.singletonSet(1) // Set {1}
+  val set2: Set = FunSets.singletonSet(2) // Set {2}
+  val set3: Set = FunSets.union(set1, set2) // Set {1, 2}
+
+  // Testing the union operation
+  println("Union of set1 and set2:")
+  FunSets.printSet(set3) // Expected: {1, 2}
+
+  // Testing intersection
+  val set4: Set = FunSets.intersect(set1, set2) // Set {1} intersects with set {2}, resulting in an empty set
+  println("\nIntersection of set1 and set2:")
+  FunSets.printSet(set4) // Expected: {}
+
+  // Testing difference
+  val set5: Set = FunSets.diff(set3, set1) // Set {1, 2} - {1} = {2}
+  println("\nDifference between set3 and set1:")
+  FunSets.printSet(set5) // Expected: {2}
+
+  // Testing filter
+  val set6: Set = FunSets.filter(set3, x => x % 2 == 0) // Filtering even numbers from {1, 2}
+  println("\nFiltered set with even numbers:")
+  FunSets.printSet(set6) // Expected: {2}
+
+  // Testing forall
+  val set7: Set = FunSets.union(FunSets.singletonSet(1), FunSets.singletonSet(2)) // Set {1, 2}
+  val allPositive = FunSets.forall(set7, x => x > 0) // Checking if all elements are greater than 0
+  println(s"\nDo all elements in set7 satisfy x > 0? $allPositive") // Expected: true
+
+  // Testing exists
+  val existsEven = FunSets.exists(set7, x => x % 2 == 0) // Checking if there is an even number in {1, 2}
+  println(s"\nDoes set7 contain an even number? $existsEven") // Expected: true
+
+  // Testing map
+  val set8: Set = FunSets.map(set3, x => x * 2) // Applying multiplication by 2 to each element of {1, 2}
+  println("\nMapped set with x * 2:")
+  FunSets.printSet(set8) // Expected: {2, 4}
+
 }
