e705a026787f1c6d405de5e39619d0835d1fcd54
Кощей бесстыжий Ayzek <thisisnauchno@gmail.com>
2024-12-03 20:14:49 +0300

    Сделано (только файлы scala)

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,22 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c-1,r-1) + pascal(c, r-1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def cyc(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty) open == 0
+      else if (open < 0) false
+      else if (chars.head == '(' || chars.head == '[' || chars.head == '{') cyc(chars.tail, open + 1)
+      else if (chars.head == ')' || chars.head == ']' || chars.head == '}') cyc(chars.tail, open - 1)
+      else cyc(chars.tail, open)
+    }
+    cyc(chars,0)
   }
 
   /**
@@ -33,6 +41,9 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money < 0) 0
+    else if (money == 0) 1
+    else if (coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
