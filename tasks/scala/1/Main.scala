cbc77a5abadcfaf3c6665cca77334f375adde3b7
dwezis <emax151004@gmail.com>
2024-12-03 15:18:11 +0300

    Update Main.scala
    First task

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,38 +1,48 @@
 package recfun
-import common._
 
 object Main {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+
+    println(balance(")(".toList)) // false
+    println(balance("a(b)c".toList)) // true
+    println(balance("()()(()())".toList)) // true
+    println(balance("()()((())".toList)) // false
+
+    println(countChange(12, List(2, 3, 5, 10))) // 6
   }
 
-  /**
-   * Exercise 1
-   */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
-  /**
-   * Exercise 2 Parentheses Balancing
-   */
   def balance(chars: List[Char]): Boolean = {
-   
+    def inner(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty) open == 0
+      else if (open < 0) false
+      else chars.head match {
+        case '(' => inner(chars.tail, open + 1)
+        case ')' => inner(chars.tail, open - 1)
+        case _ => inner(chars.tail, open)
+      }
+    }
+
+    inner(chars, 0)
   }
 
-  /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
-   */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def countRecursive(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money < 0 || coins.isEmpty) 0
+      else countRecursive(money - coins.head, coins) + countRecursive(money, coins.tail)
+    }
 
+    countRecursive(money, coins.sorted)
   }
 }
