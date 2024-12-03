dc1a8dfeef373bc72b836adda5fd8d7d061298ef
Georgy Zoloev <gzoloev228@gmail.com>
2024-12-03 16:07:07 +0300

    completed second_lab

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -2,5 +2,13 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+  val s1 = singletonSet(1)
+  val s2 = singletonSet(2)
+  val s3 = union(s1, s2)
+  val s4 = filter(s3, x => x % 2 == 0)
+
+  println(contains(s1, 1)) // true
+  println(contains(s2, 1)) // false
+  println(toString(s3))    // {1,2}
+  println(toString(s4))    // {2}
 }
