34d4da097a3a13c4df8307e623ed6671671b8e7f
pmlobanov <lobanovpm@yandex.ru>
2024-12-09 01:55:12 +0300

    feat: implementation of 2 tasks added

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,38 +1,77 @@
 package recfun
-import common._
+//import common._
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
+    //println(getPascalValue (1 ,1))
+    println("--Pascal's Triangle--")
     for (row <- 0 to 10) {
-      for (col <- 0 to row)
+      for (col <- 0 to row) {
         print(pascal(col, row) + " ")
+        //println(col,row);
+      }
       println()
     }
+    println()
+    println("--Parentheses Balancing--")
+    print("Is ()(( balanced?: ")
+    println(balance(List('(', ')', '(', '(')))
+    print("Is ()() balanced?: ")
+    println(balance(List('(', ')', '(', ')')))
+    print("Is ()a( balanced?: ")
+    println(balance(List('(', ')', 'a', '(')))
+    println()
+    println("--Counting Change--")
+    print("Change for 5 with 3,5: ")
+    println(countChange(5, List(3,5)))
+    print("Change for 5 with 1,2,3: ")
+    println(countChange(5, List(1,2,3)))
+    print("Change for 4 with 1,2: ")
+    println(countChange(4, List(1,2)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ( c<0 || c > r) 0
+    else if (c == r || c == 0) 1
+    else  pascal(c - 1, r - 1) + pascal(c , r-1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
+
   def balance(chars: List[Char]): Boolean = {
-   
+    def get_balance (chairs: List[Char], count: Int): Boolean = {
+      if (chairs.isEmpty) {
+        count match {
+          case 0 => true
+          case _ => false
+        }
+      }
+      else {
+        chairs.head match {
+          case '(' => get_balance(chairs.tail, count + 1)
+          case ')' => get_balance(chairs.tail, count - 1)
+          case _ => get_balance(chairs.tail, count)
+        }
+      }
+    }
+    get_balance(chars, 0)
   }
 
   /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
-   */
+   //   * Exercise 3 Counting Change
+   //   * Write a recursive function that counts how many different ways you can make
+   //   * change for an amount, given a list of coin denominations. For example,
+   //   * there is 1 way to give change for 5 if you have coins with denomiation
+   //   * 2 and 3: 2+3.
+   //   */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money ==0) 1
+    else if (money <0 || coins.isEmpty) 0
+    else countChange(money - coins.reverse.head, coins ) + countChange(money, coins.reverse.tail)
   }
 }
--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -1,91 +1,93 @@
 package funsets
 
-import common._
+//import common._
 
 /**
- * 2. Purely Functional Sets.
- */
+* 2. Purely Functional Sets.
+*/
 object FunSets {
   /**
-   * We represent a set by its characteristic function, i.e.
-   * its `contains` predicate.
-   */
+  * We represent a set by its characteristic function, i.e.
+    * its contains predicate.
+  */
   type Set = Int => Boolean
 
   /**
-   * Indicates whether a set contains a given element.
-   */
+  * Indicates whether a set contains a given element.
+    */
   def contains(s: Set, elem: Int): Boolean = s(elem)
 
   /**
-   * Returns the set of the one given element.
-   */
-    def singletonSet(elem: Int): Set = ???
-  
+  * Returns the set of the one given element.
+    */
+  def singletonSet(elem: Int): Set = (x: Int) => x== elem
+
 
   /**
-   * Returns the union of the two given sets,
-   * the sets of all elements that are in either `s` or `t`.
-   */
-    def union(s: Set, t: Set): Set = ???
-  
-  /**
-   * Returns the intersection of the two given sets,
-   * the set of all elements that are both in `s` and `t`.
-   */
-    def intersect(s: Set, t: Set): Set = ???
-  
-  /**
-   * Returns the difference of the two given sets,
-   * the set of all elements of `s` that are not in `t`.
-   */
-    def diff(s: Set, t: Set): Set = ???
-  
-  /**
-   * Returns the subset of `s` for which `p` holds.
-   */
-    def filter(s: Set, p: Int => Boolean): Set = ???
-  
+  * Returns the union of the two given sets,
+  * the sets of all elements that are in either `s` or `t`.
+    */
+  def union(s: Set, t: Set): Set = (x: Int) => contains(s, x) || contains(t, x)
 
   /**
-   * The bounds for `forall` and `exists` are +/- 1000.
-   */
+  * Returns the intersection of the two given sets,
+  * the set of all elements that are both in s and t.
+    */
+  def intersect(s: Set, t: Set): Set = (x: Int) => contains(s, x) && contains(t, x)
+
+  /**
+  * Returns the difference of the two given sets,
+  * the set of all elements of `s` that are not in `t`.
+    */
+  def diff(s: Set, t: Set): Set = (x: Int) => contains(s, x) && !contains(t, x)
+
+  /**
+  * Returns the subset of s for which p holds.
+    */
+  def filter(s: Set, p: Int => Boolean): Set = (x: Int) => s(x) && p(x)
+
+
+  /**
+  * The bounds for `forall` and `exists` are +/- 1000.
+  */
   val bound = 1000
 
   /**
-   * Returns whether all bounded integers within `s` satisfy `p`.
-   */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  * Returns whether all bounded integers within s satisfy p.
+  */
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (contains(s, a) && !contains(p, a)) false
+      else iter(a+1)
     }
-    iter(???)
+    iter(-bound)
   }
-  
+
   /**
-   * Returns whether there exists a bounded integer within `s`
-   * that satisfies `p`.
-   */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+  * Returns whether there exists a bounded integer within `s`
+    * that satisfies `p`.
+  */
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s,x => !p(x))
+
   /**
-   * Returns a set transformed by applying `f` to each element of `s`.
-   */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+  * Returns a set transformed by applying f to each element of s.
+    */
+  def map(s: Set, f: Int => Int): Set = {
+    (elem: Int) => exists(s, {(another: Int) => elem == f(another)})
+  }
+
   /**
-   * Displays the contents of a set
-   */
+  * Displays the contents of a set
+  */
   def toString(s: Set): String = {
     val xs = for (i <- -bound to bound if contains(s, i)) yield i
     xs.mkString("{", ",", "}")
   }
 
   /**
-   * Prints the contents of a set on the console.
-   */
+  * Prints the contents of a set on the console.
+  */
   def printSet(s: Set) {
     println(toString(s))
   }
