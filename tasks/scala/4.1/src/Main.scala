7316724c1d34e09be328e98d1e9c117162b9f343
polyalugovenko <polinalugovenko0403@gmail.com>
2024-12-15 00:42:51 +0300

    Задание 4.1 по Scala

---
+++ tasks/scala/4.1/src/Main.scala
@@ -0,0 +1,24 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    val v1 = Vector3D(1, 2, 3)
+    val v2 = Vector3D(4, 5, 6)
+    println(s"v1 + v2 = ${v1.addition(v2)}")
+    println(s"v1 - v2 = ${v1.substraction(v2)}")
+    println(s"v1 * 4 = ${v1.multiplication(4)}")
+    println(s"v1 * v2 = ${v1.multiplicationScalar(v2)}")
+    println(s"v1 Transpose: ${v1.transpose()}")
+  }
+}
+
+case class Vector3D(x:Int, y:Int, z:Int){
+  def addition(second: Vector3D): Vector3D = Vector3D(this.x + second.x, this.y + second.y, this.z + second.z)
+
+  def substraction(second: Vector3D): Vector3D = Vector3D(this.x - second.x, this.y - second.y, this.z - second.z)
+
+  def multiplication(scalar: Int): Vector3D = Vector3D(this.x * scalar, this.y * scalar, this.z * scalar)
+
+  def multiplicationScalar(second: Vector3D): Vector3D = Vector3D(this.x * second.x, this.y * second.y, this.z * second.z)
+
+  def transpose(): List[Int] = List (x,y,z)
+
+}
