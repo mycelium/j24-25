6d741c193b9cea1944487276ff59bf52d2db13f6
AkselLove <135027919+AkselLove@users.noreply.github.com>
2024-12-16 19:56:12 +0300

    FunSets.scala

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -7,76 +7,75 @@ import common._
  */
 object FunSets {
   /**
-   * We represent a set by its characteristic function, i.e.
-   * its `contains` predicate.
+   * Мы представляем множество как его характеристическую функцию, т.е.
+   * предикат `contains`.
    */
   type Set = Int => Boolean
 
   /**
-   * Indicates whether a set contains a given element.
+   * Проверяет, содержит ли множество `s` элемент `elem`.
    */
   def contains(s: Set, elem: Int): Boolean = s(elem)
 
   /**
-   * Returns the set of the one given element.
+   * Возвращает множество, содержащее только один заданный элемент.
    */
-    def singletonSet(elem: Int): Set = ???
-  
+  def singletonSet(elem: Int): Set = (x: Int) => x == elem
 
   /**
-   * Returns the union of the two given sets,
-   * the sets of all elements that are in either `s` or `t`.
+   * Возвращает объединение двух множеств `s` и `t`,
+   * т.е. множество всех элементов, которые находятся либо в `s`, либо в `t`.
    */
-    def union(s: Set, t: Set): Set = ???
-  
-  /**
-   * Returns the intersection of the two given sets,
-   * the set of all elements that are both in `s` and `t`.
-   */
-    def intersect(s: Set, t: Set): Set = ???
-  
-  /**
-   * Returns the difference of the two given sets,
-   * the set of all elements of `s` that are not in `t`.
-   */
-    def diff(s: Set, t: Set): Set = ???
-  
-  /**
-   * Returns the subset of `s` for which `p` holds.
-   */
-    def filter(s: Set, p: Int => Boolean): Set = ???
-  
+  def union(s: Set, t: Set): Set = (x: Int) => s(x) || t(x)
 
   /**
-   * The bounds for `forall` and `exists` are +/- 1000.
+   * Возвращает пересечение двух множеств `s` и `t`,
+   * т.е. множество всех элементов, которые находятся одновременно в `s` и в `t`.
+   */
+  def intersect(s: Set, t: Set): Set = (x: Int) => s(x) && t(x)
+
+  /**
+   * Возвращает разность двух множеств `s` и `t`,
+   * т.е. множество всех элементов `s`, которых нет в `t`.
+   */
+  def diff(s: Set, t: Set): Set = (x: Int) => s(x) && !t(x)
+
+  /**
+   * Возвращает подмножество `s`, для которого предикат `p` выполняется.
+   */
+  def filter(s: Set, p: Int => Boolean): Set = (x: Int) => s(x) && p(x)
+
+  /**
+   * Границы для функций `forall` и `exists` - +/- 1000.
    */
   val bound = 1000
 
   /**
-   * Returns whether all bounded integers within `s` satisfy `p`.
+   * Проверяет, выполняется ли предикат `p` для всех элементов множества `s` в пределах границ.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (s(a) && !p(a)) false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound)
   }
-  
+
   /**
-   * Returns whether there exists a bounded integer within `s`
-   * that satisfies `p`.
+   * Проверяет, существует ли элемент в множестве `s`, для которого предикат `p` выполняется.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    !forall(s, (x: Int) => !p(x))
+  }
+
   /**
-   * Returns a set transformed by applying `f` to each element of `s`.
+   * Возвращает множество, полученное применением функции `f` к каждому элементу множества `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+  def map(s: Set, f: Int => Int): Set = (y: Int) => exists(s, (x: Int) => f(x) == y)
+
   /**
-   * Displays the contents of a set
+   * Отображает содержимое множества в виде строки.
    */
   def toString(s: Set): String = {
     val xs = for (i <- -bound to bound if contains(s, i)) yield i
@@ -84,7 +83,7 @@ object FunSets {
   }
 
   /**
-   * Prints the contents of a set on the console.
+   * Выводит содержимое множества на консоль.
    */
   def printSet(s: Set) {
     println(toString(s))
