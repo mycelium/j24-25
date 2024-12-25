2c0bb47dda7442e486073e9a6d3afb033ce470bc
дмитрий губковский <gubkovskij.dv@edu.spbstu.ru>
2024-12-25 17:32:58 +0300

    Add second function

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -8,6 +8,12 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println(balance(("()()((()))()()((()))").toList))
+    println(balance(("()()((()))()()((()))(").toList))
+    println(balance(("()()((()))()()((())))").toList))
+    println(balance(("(x + 1) * 2 = 2 * x + 2").toList))
+    println(balance(("(x + 1) * 2 = (2 * x + 2").toList))
   }
 
   /**
@@ -21,10 +27,19 @@ object Main {
   /**
    * Exercise 2 Parentheses Balancing
    */
-  //  def balance(chars: List[Char]): Boolean = {
-  //
-  //  }
+  def balance(chars: List[Char]): Boolean = {
+    val r = countBrackets(0, 0, chars)
+    (r.head == r.last)
+  }
+  def countBrackets(rightBrackets: Int, leftBrackets: Int, chars: List[Char]): List[Int] = {
+    if (chars.isEmpty) List(rightBrackets, leftBrackets)
+    else chars.head match {
+      case ')' => countBrackets(rightBrackets + 1, leftBrackets, chars.tail)
+      case '(' => countBrackets(rightBrackets, leftBrackets + 1, chars.tail)
+      case _ => countBrackets(rightBrackets, leftBrackets, chars.tail)
+    }
 
+  }
   /**
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
