6bad0859c4ccd64288af4d8ae23b2d336c643a23
Pluzhnikova Julia <pluzhnikova.yus@edu.spbstu.ru>
2024-12-25 17:10:10 +0300

    Task 2 is completed

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -20,32 +20,36 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
+    def singletonSet(elem: Int): Set =
+      (x: Int) => x == elem
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set =
+      (x: Int) => s(x) || t(x)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set =
+      (x: Int) => s(x) && t(x)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set =
+      (x: Int) => s(x) && !t(x)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
-  
+    def filter(s: Set, p: Int => Boolean): Set =
+      (x: Int) => s(x) && p(x)
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -56,24 +60,27 @@ object FunSets {
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
-    }
-    iter(???)
+      def iter(a: Int): Boolean = {
+        if (a > bound) true
+        else if (s(a) && !p(a)) false
+        else iter(a + 1)
+      }
+
+      iter(-bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean =
+      !forall(s, x => !p(x))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+    def map(s: Set, f: Int => Int): Set =
+      (y: Int) => exists(s, x => f(x) == y)
   
   /**
    * Displays the contents of a set
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,31 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  println("union")
+  println(FunSets.toString(union(singletonSet(4), singletonSet(6))))
+
+  println("intersect")
+  println(FunSets.toString(intersect(singletonSet(1), singletonSet(2))))
+  println(FunSets.toString(intersect(union(singletonSet(4), singletonSet(2)), singletonSet(4))))
+
+  println("diff")
+  println(FunSets.toString(diff(union(singletonSet(4), singletonSet(2)),
+    union(singletonSet(4), singletonSet(3)))))
+
+  println("filter")
+  println(FunSets.toString(filter(union(singletonSet(-3), singletonSet(5)), (x: Int) => x > 0)))
+
+  println("forall")
+  println(FunSets.toString((x: Int) => x >= -100 && x % 10 == 0 && x <= 100))
+  println(forall((x: Int) => x >= -100 && x % 10 == 0 && x <= 100, (x: Int) => x % 2 == 0))
+
+  println("exists")
+  println(FunSets.toString((x: Int) => x >= -100 && x % 10 == 0 && x <= 100))
+  println(exists((x: Int) => x >= -100 && x % 10 == 0 && x <= 100, (x: Int) => x == 90))
+
+  println("map")
+  println(FunSets.toString((x: Int) => x >= 0 && x <= 10))
+  println(FunSets.toString(map((x: Int) => x >= 0 && x <= 10, (x: Int) => x + 2)))
+
 }
