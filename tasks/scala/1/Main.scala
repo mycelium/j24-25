596ef574c862bcf0e2f0d2c1339573b5c84af6ca
Melenkaa <aa.melenka@gmail.com>
2025-01-27 17:32:35 +0300

    Added Scala labs files

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,22 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def balanceHelper(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty) count == 0
+      else if (chars.head == '(') balanceHelper(chars.tail, count + 1)
+      else if (chars.head == ')') count > 0 && balanceHelper(chars.tail, count - 1)
+      else balanceHelper(chars.tail, count)
+    }
+
+    balanceHelper(chars, 0)
   }
 
   /**
@@ -33,6 +41,8 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
