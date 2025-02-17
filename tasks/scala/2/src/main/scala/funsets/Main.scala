e7cebe4cf65eae4748974db6d5c33371ac678deb
Arity-T <tish.mainmail@gmail.com>
2025-02-17 12:26:59 +0300

    intersect

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -9,4 +9,13 @@ object Main extends App {
   println("1 in oneTwoSet - " + contains(oneTwoSet, 1))
   println("2 in oneTwoSet - " + contains(oneTwoSet, 2))
   println("3 in oneTwoSet - " + contains(oneTwoSet, 3))
+
+  println("Create set twoThreeSet = {2} U {3}")
+  val twoThreeSet = union(singletonSet(2), singletonSet(3))
+
+  println("Create oneTwoSet and twoThreeSet intersection")
+  val intersectSet = intersect(oneTwoSet, twoThreeSet)
+  println("1 in oneTwoSet intersect twoThreeSet - " + contains(intersectSet, 1))
+  println("2 in oneTwoSet intersect twoThreeSet - " + contains(intersectSet, 2))
+  println("3 in oneTwoSet intersect twoThreeSet - " + contains(intersectSet, 3))
 }
