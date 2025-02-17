37890c3b82fed8548b80a1a8884110ee28278d93
Arity-T <tish.mainmail@gmail.com>
2025-02-17 12:21:46 +0300

    Union

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,10 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  println("Create set oneTwoSet = {1} U {2}")
+  val oneTwoSet = union(singletonSet(1), singletonSet(2))
+  println("1 in oneTwoSet - " + contains(oneTwoSet, 1))
+  println("2 in oneTwoSet - " + contains(oneTwoSet, 2))
+  println("3 in oneTwoSet - " + contains(oneTwoSet, 3))
 }
