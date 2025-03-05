d34555705393b9db54cdbcf5f7c8767abebccf15
StanleyStanMarsh <iastafev56@gmail.com>
2025-03-05 15:11:08 +0300

    docs: add documentation for JsonInteractor interface

--- tasks/java/1/json-parser/src/main/java/ru/spbstu/telematics/json/jsoninteraction/JsonInteractor.java
+++ tasks/java/1/json-parser/src/main/java/ru/spbstu/telematics/json/jsoninteraction/JsonInteractor.java
@@ -3,7 +3,17 @@ package ru.spbstu.telematics.json.jsoninteraction;
 import java.io.*;
 import java.util.Map;
 
+/**
+ * Interface for interacting with JSONs
+ * It contains methods for writing Map of (String, Object) into JSON string and
+ * reading files into JSON string
+ */
 public interface JsonInteractor {
+    /**
+     * Write Map into JSON formatted string
+     * @param map Map of (String, Object) that will be written
+     * @return JSON formatted string
+     */
     static String mapToJson(Map<String, Object> map) {
         StringBuilder json = new StringBuilder("{");
         for (Map.Entry<String, Object> entry : map.entrySet()) {
@@ -22,6 +32,12 @@ public interface JsonInteractor {
         return json.toString();
     }
 
+    /**
+     * Reads file into string
+     * @param jsonFile opening file
+     * @return string that contains content of the jsonFile
+     * @throws IOException when I/O error occurs while reading file
+     */
     static String jsonFileToJsonString(File jsonFile) throws IOException {
         if (jsonFile == null) {
             throw new NullPointerException("The file is null");
