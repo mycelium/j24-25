96d2d09bb6d5990c9fbb191fabdf9682a8259f80
Anton Lukashin <anton.lukashin@crplab.ru>
2025-03-10 11:43:19 +0300

    2025-03-10 Client-Server app. Initial project

---
+++ classroom/j2425-2/src/main/java/ru/spbstu/Main.java
@@ -0,0 +1,22 @@
+package ru.spbstu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.spbstu.server.HttpClient;
+import ru.spbstu.server.HttpServer;
+
+import java.io.IOException;
+
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Project started");
+        try {
+            var server = new HttpServer(30001);
+            HttpClient.authorize("login", "password");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
