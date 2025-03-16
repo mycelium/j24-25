f24f4223aa3e6d23212d71b398d81d78e3dceb95
tutibase <dykun1784@gmail.com>
2025-03-16 12:27:11 +0300

    feat: User class

--- tasks/java/1/json-parser/src/main/java/ru/spbstu/Main.java
+++ tasks/java/1/json-parser/src/main/java/ru/spbstu/Main.java
@@ -1,14 +1,28 @@
 package main.java.ru.spbstu;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         JsonParser parser = new JsonParser();
 
-        String json = "{\"name\":\"John\",\"age\":22,\"isStudent\":null,\"hobbies\":[\"reading\",\"coding\"],\"age2\":30, \"intArr\":[1,2]}";
+        String json = "{\"name\":\"John\",\"age\":22,\"isStudent\":null,\"hobbies\":[\"reading\",\"coding\"]}";
         String json2 = "{address:[1, 2, { nestedKey: [true, false] }, [3,4]],\"array\":[1,\"12\",3,14,15,61], \"bool\":false}";
+
         Map<String, Object> map = parser.fromJsonToMap(json2);
         System.out.println(map);
+    }
+}
+
+class User {
+    private String name;
+    private Integer age;
+    private Boolean isStudent;
+    private String[] hobbies;
+
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', age=" + age + ", isStudent=" + isStudent + ", hobbies=" + Arrays.toString(hobbies) + "}";
     }
 }
