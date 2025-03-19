f99e7cdfdb47cc973b22b87aac9bf83efdb526b3
Arity-T <tish.mainmail@gmail.com>
2025-03-19 22:37:13 +0300

    Character to json

--- tasks/java/1/tson/lib/src/main/java/dev/tishenko/Tson.java
+++ tasks/java/1/tson/lib/src/main/java/dev/tishenko/Tson.java
@@ -1,9 +1,32 @@
 package dev.tishenko;
 
+import java.util.Map;
+
 public class Tson {
+    static final Map<Character, String> SPECIAL_CHARACTERS = Map.of(
+            '"', "\\\"",
+            '\\', "\\\\",
+            '\b', "\\b",
+            '\f', "\\f",
+            '\n', "\\n",
+            '\r', "\\r",
+            '\t', "\\t");
+
+    private String characterToJson(Character c) {
+        if (SPECIAL_CHARACTERS.containsKey(c)) {
+            return "\"" + SPECIAL_CHARACTERS.get(c) + "\"";
+        }
+
+        return "\"" + c + "\"";
+    }
+
     public String toJson(Object obj) {
         if (obj == null) {
             return "null";
+        }
+
+        if (obj instanceof Character) {
+            return characterToJson((Character) obj);
         }
 
         return obj.toString();
