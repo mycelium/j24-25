727abec59303e354da13bd8a268a97114f75dc86
Arity-T <tish.mainmail@gmail.com>
2025-03-20 19:03:22 +0300

    Reordered imports in maps tests

--- tasks/java/1/tson/lib/src/test/java/dev/tishenko/MapsToJsonTest.java
+++ tasks/java/1/tson/lib/src/test/java/dev/tishenko/MapsToJsonTest.java
@@ -1,13 +1,9 @@
 package dev.tishenko;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
 
 public class MapsToJsonTest {
     @Test
