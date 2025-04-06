d44135ac5c48469501bd82779b14dd147b3b18d4
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-04-06 19:25:44 +0300

    Add read Character and other Number to JSON

--- tasks/java/1/lab1/src/main/java/lab1/CatSON/CatSON.java
+++ tasks/java/1/lab1/src/main/java/lab1/CatSON/CatSON.java
@@ -1,29 +1,49 @@
 package lab1.CatSON;
+import java.util.Map;
 
 public class CatSON {
 
-    public static String toJson(Object obj){
-        if (obj == null){
+    private static final Map<Character, String> MAP_OF_SOME_ISO_SYMBOLS = Map.of(
+            '"', "\\\"",
+            '\\', "\\\\",
+            '\b', "\\b",
+            '\f', "\\f",
+            '\n', "\\n",
+            '\r', "\\r",
+            '\t', "\\t");
+
+
+    public String toJson(Object obj) {
+        if (obj == null) {
             return "null";
         }
 
-        if (obj instanceof Double){
-            if(((Double) obj).isNaN() || ((Double) obj).isInfinite()){
+        if (obj instanceof Number) {
+            if (obj instanceof Double && (((Double) obj).isNaN() || ((Double) obj).isInfinite())) {
                 throw new IllegalArgumentException("Nan or Infinite value can`t be read to JSON");
             }
-            System.out.println("nt pr");
-            return obj.toString();
-        }
-
-        if (obj instanceof Float){
-            if(((Float) obj).isNaN() || ((Float) obj).isInfinite()){
+            if (obj instanceof Float && (((Float) obj).isNaN() || ((Float) obj).isInfinite())) {
                 throw new IllegalArgumentException("Nan or Infinite value can`t be read to JSON");
             }
             return obj.toString();
         }
 
+        if (obj instanceof Character) {
+            return readCharacterToJson((Character) obj);
+        }
 
         return "";
+    }
+
+
+    private String readCharacterToJson(Character obj) {
+
+        if (Character.isISOControl(obj)) {
+            if(MAP_OF_SOME_ISO_SYMBOLS.containsKey(obj))
+                return "\"" + MAP_OF_SOME_ISO_SYMBOLS.get(obj) + "\"";
+            return "\"" + String.format("\\u%04x", (int) obj) + "\"";
+        }
+        return "\"" + obj + "\"";
     }
 
 }
