0464c5b0274cf7e56ab7f2957e95fc8f3efa7c98
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-04-06 23:57:34 +0300

    Add read Array to JSON

--- tasks/java/1/lab1/src/main/java/lab1/CatSON/CatSON.java
+++ tasks/java/1/lab1/src/main/java/lab1/CatSON/CatSON.java
@@ -1,39 +1,53 @@
 package lab1.CatSON;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.StringJoiner;
 
 public class CatSON {
 
-    private static final Map<Character, String> MAP_OF_SOME_ISO_SYMBOLS = Map.of('"', "\\\"", '\\', "\\\\", '\b', "\\b", '\f', "\\f", '\n', "\\n", '\r', "\\r", '\t', "\\t");
+    private static final Map<Character, String> MAP_OF_SOME_ISO_SYMBOLS =
+            Map.of('"', "\\\"",
+                    '\\', "\\\\",
+                    '\b', "\\b",
+                    '\f', "\\f",
+                    '\n', "\\n",
+                    '\r', "\\r",
+                    '\t', "\\t");
 
 
     public String toJson(Object obj) {
-        switch (obj) {
-            case null -> {
-                return "null";
+
+        if (obj == null) {
+            return "null";
+        }
+        if (obj instanceof Number) {
+            if (obj instanceof Double && (((Double) obj).isNaN() || ((Double) obj).isInfinite())) {
+                throw new IllegalArgumentException("Nan or Infinite value can`t be read to JSON");
             }
-            case Number number -> {
-                if (obj instanceof Double && (((Double) obj).isNaN() || ((Double) obj).isInfinite())) {
-                    throw new IllegalArgumentException("Nan or Infinite value can`t be read to JSON");
-                }
-                if (obj instanceof Float && (((Float) obj).isNaN() || ((Float) obj).isInfinite())) {
-                    throw new IllegalArgumentException("Nan or Infinite value can`t be read to JSON");
-                }
-                return obj.toString();
+            if (obj instanceof Float && (((Float) obj).isNaN() || ((Float) obj).isInfinite())) {
+                throw new IllegalArgumentException("Nan or Infinite value can`t be read to JSON");
             }
-            case Character c -> {
-                return readCharacterToJson(c);
-            }
-            case String str -> {
-                return readStringToJson(str);
-            }
-            default -> {
-            }
+            return obj.toString();
+        }
+        if (obj instanceof Boolean) {
+            return obj.toString();
+        }
+        if (obj instanceof Character) {
+            return readCharacterToJson((Character) obj);
+        }
+        if (obj instanceof String) {
+            return readStringToJson((String) obj);
+        }
+        if (obj.getClass().isArray()){
+            return readArrayToJson(obj);
         }
 
-        return "";
+        return "err";
+
     }
 
 
@@ -51,11 +65,20 @@ public class CatSON {
                 obj.chars()
                         .mapToObj(c -> (char) c)
                         .map(el -> Character.isISOControl(el) ?
-                        MAP_OF_SOME_ISO_SYMBOLS.containsKey(el) ?
-                        MAP_OF_SOME_ISO_SYMBOLS.get(el) :
-                        String.format("\\u%04x", (int) el) :
-                        String.valueOf(el))
+                                MAP_OF_SOME_ISO_SYMBOLS.containsKey(el) ?
+                                        MAP_OF_SOME_ISO_SYMBOLS.get(el) :
+                                        String.format("\\u%04x", (int) el) :
+                                String.valueOf(el))
                         .collect(Collectors.joining())
                 + "\"";
     }
+
+    private String readArrayToJson(Object obj){
+        StringJoiner str = new StringJoiner(",", "[", "]");
+        for (int i = 0; i < Array.getLength(obj); i++){
+            str.add(this.toJson(Array.get(obj, i)));
+        }
+        return str.toString();
+    }
+
 }
