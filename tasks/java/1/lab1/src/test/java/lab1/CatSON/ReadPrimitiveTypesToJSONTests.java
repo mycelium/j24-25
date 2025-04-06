a77e2ffd4b0c31827a9884c31dd7a14691187170
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-04-07 01:00:24 +0300

    Improved the code for tests of reading primitive types to JSON

--- tasks/java/1/lab1/src/test/java/lab1/CatSON/ReadPrimitiveTypesToJSONTests.java
+++ tasks/java/1/lab1/src/test/java/lab1/CatSON/ReadPrimitiveTypesToJSONTests.java
@@ -2,7 +2,8 @@ package lab1.CatSON;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReadPrimitiveTypesToJSONTests {
     @Test
